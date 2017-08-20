0483e1606b9eaee72470ddd7d01649921f8a3a13
flowerinthenight <chew@ulimit.co>
2017-08-20 23:59:08 +0900

    Test.

--- pkg/pretty/pretty.go
+++ pkg/pretty/pretty.go
@@ -3,7 +3,9 @@ package pretty
 import (
 	"fmt"
 	"io"
+	"log"
 	"reflect"
+	"strings"
 )
 
 var Pad int = 2
@@ -44,14 +46,80 @@ func Print(w io.Writer, s interface{}, lvl, indent int) {
 				Print(w, v.Index(i).Interface(), lvl+1, indent)
 			}
 		case reflect.Ptr:
-			v := reflect.ValueOf(s).Elem()
-			if v.IsValid() {
-				Print(w, v.Interface(), lvl+1, indent)
-			} else {
-				fmt.Fprintf(w, "%snil\n", pad)
-			}
+			/*
+				v := reflect.ValueOf(s).Elem()
+				if v.IsValid() {
+					Print(w, v.Interface(), lvl+1, indent)
+				} else {
+					fmt.Fprintf(w, "%snil\n", pad)
+				}
+			*/
 		default:
 			fmt.Fprintf(w, "%s%#v\n", pad, s)
 		}
 	}
+}
+
+func JSON(v interface{}, lvl, indent int) string {
+	var add string
+	pad := Indent(lvl * indent)
+	log.Println(len(pad))
+	value := reflect.ValueOf(v)
+	switch value.Kind() {
+	case reflect.Struct:
+		if lvl == 1 {
+			add = ""
+		}
+		str := add + fullName(value.Type()) + "[" + fmt.Sprintf("%d", lvl) + "]{\n"
+		for i := 0; i < value.NumField(); i++ {
+			l := string(value.Type().Field(i).Name[0])
+			if strings.ToUpper(l) == l {
+				str += pad + value.Type().Field(i).Name + "[" + fmt.Sprintf("%d", lvl) + "]: "
+				str += JSON(value.Field(i).Interface(), lvl+1, indent)
+				str += add + ",\n"
+			}
+		}
+		str += pad + "}"
+		return str
+	case reflect.Map:
+		if len(pad) > 0 {
+			add = ""
+		}
+		str := add + "map[" + fullName(value.Type().Key()) + "]" + fullName(value.Type().Elem()) + "{\n"
+		for _, k := range value.MapKeys() {
+			str += pad + "\"" + k.String() + "\": "
+			str += JSON(value.MapIndex(k).Interface(), lvl+1, indent)
+			str += add + ",\n"
+		}
+		str += pad + "}"
+		return str
+	case reflect.Ptr:
+		if e := value.Elem(); e.IsValid() {
+			return "&" + JSON(e.Interface(), lvl+1, indent)
+		}
+		return "nil"
+	case reflect.Slice:
+		str := "[]" + fullName(value.Type().Elem()) + "{\n"
+		for i := 0; i < value.Len(); i++ {
+			str += JSON(value.Index(i).Interface(), lvl+1, indent)
+			str += ",\n"
+		}
+		str += "}"
+		return str
+	default:
+		return fmt.Sprintf("[def]%#v[def]", v)
+	}
+}
+
+func pkgName(t reflect.Type) string {
+	pkg := t.PkgPath()
+	c := strings.Split(pkg, "/")
+	return c[len(c)-1]
+}
+
+func fullName(t reflect.Type) string {
+	if pkg := pkgName(t); pkg != "" {
+		return pkg + "." + t.Name()
+	}
+	return t.Name()
 }
--- pkg/pretty/pretty_test.go
+++ pkg/pretty/pretty_test.go
@@ -1,9 +1,41 @@
 package pretty
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIndent(t *testing.T) {
 	if i := Indent(4); i != "    " {
 		t.Errorf("Expected four(4) whitespaces, got %v", i)
 	}
+}
+
+type t1 struct {
+	S string
+}
+
+type t2 struct {
+	M   map[string]string
+	I   int
+	T1  t1
+	Pt1 *t1
+	St1 []t1
+}
+
+func TestJSON(t *testing.T) {
+	mck := t2{
+		M: map[string]string{"one": "1", "two": "2"},
+		I: 100,
+		T1: t1{
+			S: "struct",
+		},
+		Pt1: &t1{
+			S: "struct pointer",
+		},
+		St1: make([]t1, 0),
+	}
+
+	mck.St1 = append(mck.St1, t1{S: "hello"})
+	fmt.Println(JSON(mck, 1, 2))
 }
