75101be588272652e3c24077a4d68c2b4dea38af
flowerinthenight <chew.esmero@mobingi.com>
2017-08-21 11:55:28 +0900

    Can input string in JSON pretty.

--- pkg/pretty/pretty.go
+++ pkg/pretty/pretty.go
@@ -19,14 +19,22 @@ func Indent(count int) string {
 // JSON returns a prettified JSON string of `v`.
 func JSON(v interface{}, indent int) string {
 	var out bytes.Buffer
+	var b []byte
 
 	pad := Indent(indent)
-	b, err := json.Marshal(v)
-	if err != nil {
-		return err.Error()
+	_, ok := v.(string)
+	if !ok {
+		tmp, err := json.Marshal(v)
+		if err != nil {
+			return err.Error()
+		}
+
+		b = tmp
+	} else {
+		b = []byte(v.(string))
 	}
 
-	err = json.Indent(&out, b, "", pad)
+	err := json.Indent(&out, b, "", pad)
 	if err != nil {
 		return err.Error()
 	}
