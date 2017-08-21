edf7d86fb212fa246c923f4a22e2b122234dd52a
flowerinthenight <chew.esmero@mobingi.com>
2017-08-21 15:27:31 +0900

    Http client.

---
+++ client/client_test.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewSimpleHttpClient(t *testing.T) {
+	c := NewSimpleHttpClient(0, false, nil)
+	if c == nil {
+		t.Errorf("Expected an object, received nil")
+	}
+}
+
+func TestDo(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	}))
+
+	defer ts.Close()
+	c := NewSimpleHttpClient(0, false, nil)
+	r, err := http.NewRequest(http.MethodGet, ts.URL+"/test", nil)
+	if err != nil {
+		t.Errorf("New request failed: %#v", err)
+	}
+
+	resp, body, err := c.Do(r.Context(), r)
+	if err != nil {
+		t.Errorf("Do failed: %#v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Expected 200, received %v", resp.StatusCode)
+	}
+
+	if string(body) != "hello" {
+		t.Errorf("Expected body 'hello', received %s", string(body))
+	}
+}
