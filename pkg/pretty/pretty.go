9e12e4e21144e718c113ae86968f5f051d61726b
flowerinthenight <chew.esmero@gmail.com>
2017-08-21 11:27:16 +0900

    JSON prettifier (#1)
    * Test.
    
    * JSON prettifier.

--- pkg/pretty/pretty.go
+++ pkg/pretty/pretty.go
@@ -1,9 +1,8 @@
 package pretty
 
 import (
-	"fmt"
-	"io"
-	"reflect"
+	"bytes"
+	"encoding/json"
 )
 
 var Pad int = 2
@@ -17,41 +16,20 @@ func Indent(count int) string {
 	return pad
 }
 
-// Print prints the `field: value` of the input interface recursively. Recursion
-// level `lvl` and `indent` are provided for indention in printing.
-func Print(w io.Writer, s interface{}, lvl, indent int) {
-	pad := Indent(lvl * indent)
-	rt := reflect.TypeOf(s).Elem()
-	rv := reflect.ValueOf(s).Elem()
-	for i := 0; i < rt.NumField(); i++ {
-		field := rt.Field(i).Name
-		value := rv.Field(i).Interface()
-		switch rv.Field(i).Kind() {
-		case reflect.Interface:
-			fmt.Fprintf(w, "%s%s: %v\n", pad, field, value)
-		case reflect.String:
-			fmt.Fprintf(w, "%s%s: %s\n", pad, field, value)
-		case reflect.Int32:
-			fmt.Fprintf(w, "%s%s: %i\n", pad, field, value)
-		case reflect.Struct:
-			fmt.Fprintf(w, "%s[%s]\n", pad, field)
-			v := rv.Field(i).Addr()
-			Print(w, v.Interface(), lvl+1, indent)
-		case reflect.Slice:
-			fmt.Fprintf(w, "%s[%s]\n", pad, field)
-			v := reflect.ValueOf(s)
-			for i := 0; i < v.Len(); i++ {
-				Print(w, v.Index(i).Interface(), lvl+1, indent)
-			}
-		case reflect.Ptr:
-			v := reflect.ValueOf(s).Elem()
-			if v.IsValid() {
-				Print(w, v.Interface(), lvl+1, indent)
-			} else {
-				fmt.Fprintf(w, "%snil\n", pad)
-			}
-		default:
-			fmt.Fprintf(w, "%s%#v\n", pad, s)
-		}
+// JSON returns a prettified JSON string of `v`.
+func JSON(v interface{}, indent int) string {
+	var out bytes.Buffer
+
+	pad := Indent(indent)
+	b, err := json.Marshal(v)
+	if err != nil {
+		return err.Error()
 	}
+
+	err = json.Indent(&out, b, "", pad)
+	if err != nil {
+		return err.Error()
+	}
+
+	return out.String()
 }
