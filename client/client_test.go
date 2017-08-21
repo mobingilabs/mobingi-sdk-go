8ba2b53f2d8308b0cf374f151b35cae8d974ae07
flowerinthenight <chew.esmero@mobingi.com>
2017-08-21 18:28:55 +0900

    Timeout test.

--- client/client_test.go
+++ client/client_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestNewSimpleHttpClient(t *testing.T) {
@@ -36,5 +37,24 @@ func TestDo(t *testing.T) {
 
 	if string(body) != "hello" {
 		t.Errorf("Expected body 'hello', received %s", string(body))
+	}
+}
+
+func TestDoWithTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 1)
+		w.Write([]byte("hello"))
+	}))
+
+	defer ts.Close()
+	c := NewSimpleHttpClient(&Config{Timeout: time.Second * 2})
+	r, err := http.NewRequest(http.MethodGet, ts.URL+"/test", nil)
+	if err != nil {
+		t.Errorf("New request failed: %#v", err)
+	}
+
+	_, _, err = c.Do(r)
+	if err != nil {
+		t.Errorf("Do failed: %#v", err)
 	}
 }
