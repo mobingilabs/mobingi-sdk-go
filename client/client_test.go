6a9cca6de9a3f3222362d791ffe4ef0bc614eb78
flowerinthenight <chew.esmero@mobingi.com>
2017-08-21 18:47:57 +0900

    Verbose test.

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"bufio"
+	"bytes"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -56,5 +59,33 @@ func TestDoWithTimeout(t *testing.T) {
 	_, _, err = c.Do(r)
 	if err != nil {
 		t.Errorf("Do failed: %#v", err)
+	}
+}
+
+func TestDoVerbose(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	}))
+
+	defer ts.Close()
+
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	logger := log.New(w, "TEST", 0)
+
+	c := NewSimpleHttpClient(&Config{Verbose: true, Logger: logger})
+	r, err := http.NewRequest(http.MethodGet, ts.URL+"/test", nil)
+	if err != nil {
+		t.Errorf("New request failed: %#v", err)
+	}
+
+	_, _, err = c.Do(r)
+	if err != nil {
+		t.Errorf("Do failed: %#v", err)
+	}
+
+	w.Flush()
+	if b.Len() == 0 {
+		t.Errorf("Buffer should not be empty")
 	}
 }
