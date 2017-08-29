ff08845c6d65a55217231bf6987d65eb7d2946ea
flowerinthenight <chew.esmero@mobingi.com>
2017-08-29 11:23:01 +0900

    Add json check.

---
+++ pkg/filetype/filetype_test.go
@@ -0,0 +1,13 @@
+package filetype
+
+import "testing"
+
+func TestIsJSON(t *testing.T) {
+	if !IsJSON(`{"name":"hello"}`) {
+		t.Errorf("Should be json")
+	}
+
+	if !IsJSON(`[{"name":"hello"}]`) {
+		t.Errorf("Should be json")
+	}
+}
