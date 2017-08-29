b4e2e93046ca56be39fa3398d1b9611e148c6150
flowerinthenight <chew.esmero@mobingi.com>
2017-08-29 11:59:04 +0900

    Add yaml check.

--- pkg/filetype/filetype_test.go
+++ pkg/filetype/filetype_test.go
@@ -15,3 +15,30 @@ func TestIsJSON(t *testing.T) {
 		t.Errorf("Should not be json")
 	}
 }
+
+func TestIsYAML(t *testing.T) {
+	var d = `foo: 1
+hello:
+- one
+- two`
+
+	if !IsYAML(d) {
+		t.Errorf("Should be yaml")
+	}
+
+	d = `--- !test.com
+version: "some_version"
+list:
+  - val1:
+    sub: 1
+    end: true
+  - val2
+  - val3:
+    sub: 3
+    end: false
+`
+
+	if !IsYAML(d) {
+		t.Errorf("Should be yaml")
+	}
+}
