a8aadccee1f15b6d501a620bcb6d6515be87262b
flowerinthenight <chew.esmero@mobingi.com>
2017-09-01 11:59:00 +0900

    Remove linux filter.

--- pkg/nativestore/nativestore.go
+++ pkg/nativestore/nativestore.go
@@ -1,19 +1,8 @@
 package nativestore
 
-import (
-	"fmt"
-
-	dcred "github.com/docker/docker-credential-helpers/credentials"
-	"github.com/pkg/errors"
-)
+import dcred "github.com/docker/docker-credential-helpers/credentials"
 
 func Set(lbl, url, user, secret string) error {
-	switch fmt.Sprintf("%T", ns) {
-	case "osxkeychain.Osxkeychain", "wincred.Wincred":
-	default:
-		return errors.New("native store not supported yet")
-	}
-
 	cr := &dcred.Credentials{
 		ServerURL: url,
 		Username:  user,
