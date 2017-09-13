90c0e22adac3b1781ff7ed667055a62424160764
Yuto Kimura <biostudy28@gmail.com>
2017-09-13 10:52:41 +0900

    Client support for sesha3 client.
    * Initial for sesha3.
    
    * sesha3:gotty-client action
    
    * sesha3:errors.wrap
    
    * test:success
    
    * dep ensure
    
    * fix
    
    * change name
    
    * change return value
    
    * Run():change return value

---
+++ mobingi/sesha3/sesha3.go
@@ -0,0 +1,36 @@
+package sesha3
+
+import (
+	"github.com/moul/gotty-client"
+	"github.com/pkg/errors"
+)
+
+type SeshaClientInput struct {
+	URL string
+}
+
+type sesha3Client struct {
+	client *gottyclient.Client
+}
+
+func NewClient(in *SeshaClientInput) (*sesha3Client, error) {
+	var err error
+	if len(in.URL) < 1 {
+		err = errors.Wrap(err, "url should not be empty")
+		return nil, err
+	}
+	client, err := gottyclient.NewClient(in.URL)
+	if err != nil {
+		err = errors.Wrap(err, "sesha3 client creation failed")
+		return nil, err
+	}
+	return &sesha3Client{client: client}, err
+}
+
+func (c *sesha3Client) Run() error {
+	err := c.client.Loop()
+	if err != nil {
+		err = errors.Wrap(err, "can't connect sesha3")
+	}
+	return err
+}
