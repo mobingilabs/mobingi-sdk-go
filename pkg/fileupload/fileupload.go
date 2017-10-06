32d3573079bb71efcd21281ade2b178a2329ff3b
flowerinthenight <chew.esmero@mobingi.com>
2017-10-06 16:39:37 +0900

    Trial upload.

---
+++ pkg/fileupload/fileupload.go
@@ -0,0 +1,37 @@
+package fileupload
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/mobingilabs/mobingi-sdk-go/pkg/debug"
+	"github.com/pkg/errors"
+)
+
+func ProcessFileUpload(w http.ResponseWriter, r *http.Request) error {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		return errors.Wrap(err, "formfile failed")
+	}
+
+	defer file.Close()
+	path := r.FormValue("uploadpath")
+	if path == "" {
+		path = os.TempDir()
+	}
+
+	_, fstr := filepath.Split(handler.Filename)
+	debug.Info("path:", path)
+	debug.Info("file:", handler.Filename)
+	f, err := os.Create(path + "/" + fstr)
+	if err != nil {
+		return errors.Wrap(err, "create file failed")
+	}
+
+	defer f.Close()
+	_, err = io.Copy(f, file)
+	return err
+}
